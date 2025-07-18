d6ede8db88b40e8eeeb3c7b456dc17ff16ba06ce
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-18 01:07:23 +0100

    refactor

--- src/example2/index.ts
+++ src/example2/index.ts
@@ -1,38 +1,82 @@
+import assert from "node:assert";
+
 import * as html from "./html.js";
 import { type Html } from "./html.js";
 
 type Email = string;
 
-function view(email: Email) {
-  return html.div(page(email));
-}
+const expected: string = `<div>
+  <div>
+    <div>
+      <h1>
+        Our Site
+      </h1>
+    </div>
+    <div>
+      <h1>
+        Custom content for Jeremiah
+      </h1>
+      <div>
+        <p>
+          This is the left side
+        </p>
+      </div>
+      <div>
+        <div>
+          <p>
+            this is an article
+          </p>
+          <div>
+            <p>
+              Hey Jeremiah we've got a great deal for you
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
 
-function page(email: Email): Html {
-  return html.div(topNav(), content(email));
-}
+{
+  // Naive implementation without reader
 
-function topNav(): Html {
-  return html.div(html.h1("Our Site"));
-}
+  function view(email: Email) {
+    return html.div(page(email));
+  }
 
-function content(email: Email): Html {
-  return html.div(html.h1(`Custom content for ${email}`), left(), right(email));
-}
+  function page(email: Email): Html {
+    return html.div(topNav(), content(email));
+  }
 
-function left(): Html {
-  return html.div(html.p("This is the left side"));
-}
+  function topNav(): Html {
+    return html.div(html.h1("Our Site"));
+  }
 
-function right(email: Email): Html {
-  return html.div(article(email));
-}
+  function content(email: Email): Html {
+    return html.div(
+      html.h1(`Custom content for ${email}`),
+      left(),
+      right(email),
+    );
+  }
 
-function article(email: Email): Html {
-  return html.div(html.p("this is an article"), widget(email));
-}
+  function left(): Html {
+    return html.div(html.p("This is the left side"));
+  }
 
-function widget(email: Email): Html {
-  return html.div(html.p(`Hey ${email} we've got a great deal for you`));
-}
+  function right(email: Email): Html {
+    return html.div(article(email));
+  }
 
-console.log(html.render(view("Jeremiah")));
+  function article(email: Email): Html {
+    return html.div(html.p("this is an article"), widget(email));
+  }
+
+  function widget(email: Email): Html {
+    return html.div(html.p(`Hey ${email} we've got a great deal for you`));
+  }
+
+  const actual = html.render(view("Jeremiah"));
+
+  assert.strictEqual(actual, expected);
+}
